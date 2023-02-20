6d83c0ad85c232a4fd394070be95684454d2537b
Chima-Anason <tblynxx@gmail.com>
2023-02-20 14:15:32 +0800

    Initial Commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by mac on 20/02/2023.
+//  Copyright © 2023 Chima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
